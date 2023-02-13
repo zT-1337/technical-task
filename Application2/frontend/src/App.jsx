70fc61060b398ced03466478f20d3113b9a680c8
Thomas Zerr <thomas.zerr.3@hof-university.de>
2023-02-13 18:42:47 +0100

    -added: connect and disconnect feature

--- Application2/frontend/src/App.jsx
+++ Application2/frontend/src/App.jsx
@@ -1,11 +1,46 @@
 import Input from "./Input.jsx";
 import Output from "./Output.jsx";
+import io from "socket.io-client";
+import {useState} from "react";
+
+let socket;
 
 function App() {
+  const [clientId, setClientId] = useState("NOT CONNECTED");
+  const [isConnected, setIsConnected] = useState(false);
+
+  const onConnectPressed = () => {
+    socket = io("http://localhost:3000");
+    socket.on("connect", () => {
+      console.log("connected");
+    })
+
+    socket.on("disconnect", () => {
+      console.log("disconnected");
+      setIsConnected(false);
+      setClientId("NOT CONNECTED");
+
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("id");
+      socket = undefined;
+    })
+
+    socket.on("id", msg => {
+      console.log(msg);
+      setIsConnected(true);
+      setClientId(`Client Id: ${msg}`);
+    })
+  }
+
+  const onDisconnectPressed = () => {
+    socket.disconnect();
+  }
+
   return (
     <div className="container mx-auto p-4 bg-base-200 h-screen flex flex-col">
       <div className="mb-2 text-center">
-        <p>NOT CONNECTED</p>
+        <p>{clientId}</p>
       </div>
       <div className="flex flex-row flex-1">
         <div className="flex-1">
@@ -16,8 +51,18 @@ function App() {
         </div>
       </div>
       <div className="flex flex-row justify-end">
-        <button className="btn-primary btn-wide btn-md mr-8">Connect</button>
-        <button className="btn-primary btn-disabled btn-wide btn-md" disabled={true}>Disconnect</button>
+        <button 
+          className={`btn-primary btn-wide btn-md mr-8 ${isConnected ? "btn-disabled" : ""}`} 
+          onClick={onConnectPressed}
+          disabled={isConnected}>
+            Connect
+        </button>
+        <button 
+          className={`btn-primary btn-wide btn-md ${!isConnected ? "btn-disabled" : ""}`}
+          onClick={onDisconnectPressed}
+          disabled={!isConnected}>
+            Disconnect
+        </button>
       </div>
     </div>
   )
